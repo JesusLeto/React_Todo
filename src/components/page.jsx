586a5cd4e10a20cf20c375c52a89d4fb238a3e2e
JesusLeto <cghplus651@gmail.com>
2021-06-26 14:35:01 +0300

    Verst Menu

---
+++ src/components/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './menu';
+import Content from './content';
+
+const Page = () =>{
+    return (
+        <div className = "MainPage">
+            <Menu />
+            <Content />         
+        </div>
+    )
+}
+
+export default Page
